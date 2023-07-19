ec8ae155d402d854ca2bd95e160c92e15d946d3a
ulianych <uliana.ignatchik@gmail.com>
2023-07-19 15:00:47 +0300

    added functions in parsing file, main edited

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ from aiogram import Bot, Dispatcher, executor, types
 import FamcsBotMarkups as mk
 import config
 import time
+from parsing import all_posts
+from parsing import new_post
 
 TOKEN = config.TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
 # Стартовая менюшка
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
-    user_id = message.from_user.id
     await bot.send_message(message.from_user.id, 'Привет, {0.first_name}'.format(message.from_user), reply_markup=mk.startMenu)
+
+
+@dp.message_handler(text='pussy')
+async def get_list(message: types.Message):
+    total_list = all_posts()
+    await bot.send_message(message.from_user.id, total_list, parse_mode=types.ParseMode.HTML)
 
 
 # Ответ на запрос про баллы (пока сделал так, потом будем менять)
@@ -56,10 +62,15 @@ async def timetable4_call(callback: types.CallbackQuery):
     await callback.answer()
 
 
-async def message_sender(user_id):
+@dp.message_handler(commands=['pizda'])
+async def send_message(message: types.Message):
+    back_list = []
     while True:
-        await bot.send_message(user_id, 'pussy')
-        time.sleep(10)
+        post = new_post(back_list)[0]
+        back_list = new_post(back_list)[1]
+        if post is not None:
+            await bot.send_message(message.from_user.id, post, parse_mode=types.ParseMode.HTML)
+        time.sleep(1800)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- parsing.py
+++ parsing.py
@@ -1,19 +1,62 @@
-from datetime import date, datetime
+from datetime import datetime
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.linkedin.com/jobs/search?keywords=It&location=%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0%20%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C&locationId=&geoId=101705918&f_TPR=r86400&position=1&pageNum=0"
 
-response = requests.get(url)
-print(response)
-bs = BeautifulSoup(response.text, "html.parser")
+def new_post(back_list):
+    url = "https://www.linkedin.com/jobs/search?keywords=It&location=%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0%20%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C&locationId=&geoId=101705918&f_TPR=r86400&position=1&pageNum=0"
 
-list = bs.find_all(class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
-for i in list:
-    if i.find(datetime=str(datetime.now().date())) is not None:
-        print(i.find(class_='base-search-card__title').text.strip())
-        print(i.find(class_='base-search-card__subtitle').text.strip())
-        print(i.find(class_='job-search-card__location').text.strip())
-        print(i.find(class_='job-search-card__listdate--new').text.strip())
-        print(i.find('a', class_='base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]').get('href'))
-        print('\n')
+    response = requests.get(url)
+    bs = BeautifulSoup(response.text, "html.parser")
+
+    new_list = bs.find_all(class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
+
+    cleared_list = []
+    for i in new_list:
+        if i.find(datetime=str(datetime.now().date())) is not None:
+            cleared_list.append(i)
+
+    print("back = " + str(len(back_list)) + ", new = " + str(len(cleared_list)))
+
+    if len(cleared_list) != len(back_list):
+        link = cleared_list[len(cleared_list)-1].find('a', class_='base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]').get('href')
+        title = cleared_list[len(cleared_list)-1].find(class_='base-search-card__title').text.strip()
+        answer = f'<a href="{link}">{title}</a>' + "\n"
+        answer += cleared_list[len(cleared_list)-1].find(class_='base-search-card__subtitle').text.strip() + "\n"
+        answer += cleared_list[len(cleared_list)-1].find(class_='job-search-card__location').text.strip() + "\n"
+        answer += cleared_list[len(cleared_list)-1].find(class_='job-search-card__listdate--new').text.strip() + "\n"
+
+        return [answer, cleared_list]
+    else:
+        return [None, back_list]
+    '''
+    for i in list:
+        if i.find(datetime=str(datetime.now().date())) is not None:
+            print(i.find(class_='base-search-card__title').text.strip())
+            print(i.find(class_='base-search-card__subtitle').text.strip())
+            print(i.find(class_='job-search-card__location').text.strip())
+            print(i.find(class_='job-search-card__listdate--new').text.strip())
+            print(i.find('a', class_='base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]').get('href'))
+            print('\n')
+    '''
+
+
+def all_posts():
+    url = "https://www.linkedin.com/jobs/search?keywords=It&location=%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0%20%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C&locationId=&geoId=101705918&f_TPR=r86400&position=1&pageNum=0"
+
+    response = requests.get(url)
+    bs = BeautifulSoup(response.text, "html.parser")
+
+    new_list = bs.find_all(class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
+    item = ""
+
+    for i in new_list:
+        if i.find(datetime=str(datetime.now().date())) is not None:
+            link = i.find('a', class_='base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]').get('href')
+            title = i.find(class_='base-search-card__title').text.strip()
+            item += f'<a href="{link}">{title}</a>' + "\n"
+            item += i.find(class_='base-search-card__subtitle').text.strip() + "\n"
+            item += i.find(class_='job-search-card__location').text.strip() + "\n"
+            item += i.find(class_='job-search-card__listdate--new').text.strip() + "\n"
+            item += '\n'
+    return item
